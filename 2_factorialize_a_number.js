4a9a6b2dba4b3396985c6e13db51984b2a77b1bf
Ken Austria <kenn.pangilinan@gmail.com>
2017-09-26 00:30:01 -0700

    Add files via upload

---
+++ 2_factorialize_a_number.js
@@ -0,0 +1,13 @@
+function factorialize(num) {
+  // set factorial value
+  var factorial = 1;
+  
+  // iterate through num
+  for (var i = 1; i <= num; i++) {
+    // factorial equals itself times value of var i
+    factorial = factorial * i;
+  }
+  return factorial;
+}
+
+factorialize(4);
