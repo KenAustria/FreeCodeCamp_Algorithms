c86d41a54b233d3719887928a1124ab82967a5da
Ken Austria <kenn.pangilinan@gmail.com>
2017-09-26 00:25:54 -0700

    Add files via upload

---
+++ 1_reverse_a_string.js
@@ -0,0 +1,11 @@
+function reverseString(str) {
+  // host reversed string  
+  var newString = "";
+  // iterate through input from last index to first
+  for (var i = str.length - 1; i >= 0; i--) {
+    // push index character to host string   
+    newString += str[i];
+  }
+  return newString;
+}
+reverseString('hello');
