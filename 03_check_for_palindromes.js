b8c221fb6b18902dd11c90c32d21e46580fc1ba5
Ken Austria <kenn.pangilinan@gmail.com>
2017-09-30 02:43:37 -0700

    Update 03_check_for_palindromes.js

--- 03_check_for_palindromes.js
+++ 03_check_for_palindromes.js
@@ -1,3 +1,8 @@
+/*
+Return true if the given string is a palindrome, a word or sentence that's spelled 
+the same way both forward and backward, ignoring punctuation, case, and spacing.
+*/
+
 function palindrome(str) {
   // remove unwanted characters
   var re = /[\W_]/g;
