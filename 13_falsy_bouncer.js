bdaa166f12f59a7f3558ed476b09bc17c77f62e4
Ken Austria <kenn.pangilinan@gmail.com>
2017-09-30 02:01:06 -0700

    Add files via upload

---
+++ 13_falsy_bouncer.js
@@ -0,0 +1,14 @@
+function bouncer(arr) {
+  // create host array
+  var holderArray = [];
+  // call filter to run removeFalseVar method
+  holderArray = arr.filter(removeFalseVar);
+  return holderArray;
+  
+  function removeFalseVar(value) {
+    // return values for all non-falsy varaibles
+    return Boolean(value);
+  }
+}
+
+bouncer([7, "ate", "", false, 9]);
