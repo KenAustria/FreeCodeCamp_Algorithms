b05cc5dc5ae52467f189a428847558c958b03f37
Ken Austria <kenn.pangilinan@gmail.com>
2017-09-30 02:07:51 -0700

    Update 11_slasher_flick.js

--- 11_slasher_flick.js
+++ 11_slasher_flick.js
@@ -1,3 +1,8 @@
+/*
+Return the remaining elements of an array after chopping off n elements from the head,
+the beginning of the array, or the zeroth index.
+*/
+
 function slasher(arr, howMany) {
   // splice array from index[0] to value of howMany
   // return array with remaining values 
