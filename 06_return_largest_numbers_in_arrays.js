c2ed408c3e5427fee5647e9528d210fcb93f73ba
Ken Austria <kenn.pangilinan@gmail.com>
2017-09-30 02:40:20 -0700

    Update 06_return_largest_numbers_in_arrays.js

--- 06_return_largest_numbers_in_arrays.js
+++ 06_return_largest_numbers_in_arrays.js
@@ -1,3 +1,8 @@
+/*
+Return an array consisting of the largest number from each provided sub-array. 
+For simplicity, the provided array will contain exactly 4 sub-arrays.
+*/
+
 function largestOfFour(mainArray) {
   // map over the main array
   return mainArray.map(function(subArray) {
